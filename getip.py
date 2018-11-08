d1c8de6694319c38b34b8a9f24efbeea7e44f30a
Marco Berardelli <marco.berardelli@gmail.com>
2018-11-08 19:53:57 +0100

    removed unused files and folders

--- getip.py
+++
@@ -1,4 +0,0 @@
-import urllib.request
-
-ip = urllib.request.urlopen('https://api.ipify.org').read().decode('utf8')
-print(ip)
--- model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-
-type User struct {
-	Username string
-	Password string
-	Key string
-	ListCam []Cam
-}
